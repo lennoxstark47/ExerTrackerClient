ecbcd49d0e6a8888a81e2a21afbcbf642260c4e8
Stark 47 <twisamhaldar@gmail.com>
2021-07-25 00:25:07 +0530

    1st frontend try

---
+++ src/components/create-exercise.component.js
@@ -0,0 +1,141 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import axios from 'axios';
+
+export default class CreateExercise extends Component {
+	constructor() {
+		super();
+		this.state = {
+			username: '',
+			description: '',
+			duration: 0,
+			date: new Date(),
+			users: [],
+		};
+	}
+
+	componentDidMount() {
+		axios
+			.get(
+				'https://warm-beyond-25693.herokuapp.com/users/'
+			)
+			.then((response) => {
+				if (response.data.length > 0) {
+					this.setState({
+						users: response.data.map(
+							(user) => user.username
+						),
+						username: response.data[0].username,
+					});
+				}
+			})
+			.catch((err) => console.log(err));
+	}
+
+	onUsernameChange = (event) => {
+		this.setState({
+			username: event.target.value,
+		});
+	};
+	onDescriptionChange = (event) => {
+		this.setState({
+			description: event.target.value,
+		});
+	};
+	onDurationChange = (event) => {
+		this.setState({
+			duration: event.target.value,
+		});
+	};
+	onDateChange = (date) => {
+		this.setState({
+			date: date,
+		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		const exercise = {
+			username: this.state.username,
+			description: this.state.description,
+			duration: this.state.duration,
+			date: this.state.date,
+		};
+
+		console.log(exercise);
+		axios
+			.post(
+				'https://warm-beyond-25693.herokuapp.com/exercises/add/',
+				exercise
+			)
+			.then((response) =>
+				console.log(response.data)
+			);
+		// window.location = '/';
+	};
+
+	render() {
+		return (
+			<div>
+				<h3>Create New Exercise Log</h3>
+				<form onSubmit={this.handleSubmit}>
+					<div className='form-group'>
+						<label>Username: </label>
+						<select
+							useref='userInput'
+							required
+							className='form-control'
+							value={this.state.username}
+							onChange={this.onUsernameChange}>
+							{this.state.users.map((user) => {
+								return (
+									<option key={user} value={user}>
+										{user}
+									</option>
+								);
+							})}
+						</select>
+					</div>
+					<div className='form-group'>
+						<label>Description: </label>
+						<input
+							type='text'
+							required
+							className='form-control'
+							value={this.state.description}
+							onChange={this.onDescriptionChange}
+						/>
+					</div>
+					<div className='form-group'>
+						<label>Duration (in minutes:)</label>
+						<input
+							type='text'
+							required
+							className='form-control'
+							value={this.state.duration}
+							onChange={this.onDurationChange}
+						/>
+					</div>
+					<div className='form-group'>
+						<label>Date: </label>
+						<div>
+							<DatePicker
+								selected={this.state.date}
+								onChange={this.onDateChange}
+							/>
+						</div>
+					</div>
+					<div className='form-group'>
+						<input
+							type='submit'
+							value='Create Exercise Log'
+							className='btn btn-primary'
+						/>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
---
+++ src/components/create-user.component.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class CreateUser extends Component {
+	constructor() {
+		super();
+		this.state = {
+			username: '',
+		};
+	}
+
+	onUsernameChange = (event) => {
+		this.setState({
+			username: event.target.value,
+		});
+	};
+
+	handleSubmit = (event) => {
+		event.preventDefault();
+
+		const newUser = {
+			username: this.state.username,
+		};
+		console.log(newUser);
+		axios
+			.post(
+				'https://warm-beyond-25693.herokuapp.com/users/add',
+				newUser
+			)
+			.then((response) =>
+				console.log(response.data)
+			);
+		this.setState({
+			username: '',
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<h3>Create new user</h3>
+				<form onSubmit={this.handleSubmit}>
+					<div className='form-group'>
+						<label>Username: </label>
+						<input
+							type='text'
+							required
+							className='form-control'
+							onChange={this.onUsernameChange}
+							value={this.state.username}
+						/>
+					</div>
+					<div className='form-group'>
+						<input
+							type='submit'
+							className='btn btn-primary'
+							value='Create User'
+						/>
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
